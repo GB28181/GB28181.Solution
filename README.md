edff8cf7240602727ee36b8ef5c2063c302cc5ea
Eamon <chenlulei@live.com>
2018-02-06 16:26:37 +0800

    update target framework to 4.7

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Gb28181_Platform2016_Test
-国标评测公安一所
+# RegisterService :GB28181_Platform2016_MicroService
+
+## part 1 :# RegisterService
+
+~~~
+Device Registering And managemment
+~~~
+
+## part 2 :# DeviceControl
+
+~~~
+Device Controlling Service such as :PTZ 
+~~~
+
+## License
+
+BSD v2
--- SIPSorcery.28181/Properties/AssemblyInfo.cs
+++ SIPSorcery.28181/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("SIPSorcery.28181")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
