9f41380ec58c80bfaba08428b1830fd49ed6e5cd
Edward <crazybber@outlook.com>
2020-04-12 19:00:23 +0800

    batch refines codes structure design

--- StreamingEngine/TS/GB28181TSStreamInput.cs
+++ StreamingEngine/TS/GB28181TSStreamInput.cs
@@ -2,10 +2,6 @@
 using SS.Media;
 using SS.MediaServer.Media.TS;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SS.MeidaNetEngine.Media.TS
 {
