ee1edcb49545c3c6e0f0e19aa9bc6cbf12c150ce
Edward <crazybber@outlook.com>
2020-05-01 18:20:19 +0800

    add testing case

--- Testing/TestIPSocket.cs
+++ Testing/TestIPSocket.cs
@@ -7,48 +7,49 @@ using Xunit;
 namespace Testing
 {
 
-	
-	
-		public class IPSocketUnitTest
-		{
+
+
+    public class IPSocketUnitTest
+    {
+
         [Fact]
         public void SampleTest()
-			{
-				Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
-				Assert.True(true, "True was false.");
-			}
+        {
+            Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            Assert.True(true, "True was false.");
+        }
 
         [Fact]
         public void ParsePortFromSocketTest()
-            {
-                Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+        {
+            Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
 
-                int port = IPSocket.ParsePortFromSocket("localhost:5060");
-                Console.WriteLine("port=" + port);
-                Assert.True(port == 5060, "The port was not parsed correctly.");
-            }
+            int port = IPSocket.ParsePortFromSocket("localhost:5060");
+            Console.WriteLine("port=" + port);
+            Assert.True(port == 5060, "The port was not parsed correctly.");
+        }
 
         [Fact]
         public void ParseHostFromSocketTest()
-            {
-                Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+        {
+            Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
 
-                string host = IPSocket.ParseHostFromSocket("localhost:5060");
-                Console.WriteLine("host=" + host);
-                Assert.True(host == "localhost", "The host was not parsed correctly.");
-            }
+            string host = IPSocket.ParseHostFromSocket("localhost:5060");
+            Console.WriteLine("host=" + host);
+            Assert.True(host == "localhost", "The host was not parsed correctly.");
+        }
 
         [Fact]
         public void Test172IPRangeIsPrivate()
-            {
-                Console.WriteLine("--> " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        {
+            Console.WriteLine("--> " + System.Reflection.MethodBase.GetCurrentMethod().Name);
 
-                Assert.False(IPSocket.IsPrivateAddress("172.15.1.1"), "Public IP address was mistakenly identified as private.");
-                Assert.True(IPSocket.IsPrivateAddress("172.16.1.1"), "Private IP address was not correctly identified.");
+            Assert.False(IPSocket.IsPrivateAddress("172.15.1.1"), "Public IP address was mistakenly identified as private.");
+            Assert.True(IPSocket.IsPrivateAddress("172.16.1.1"), "Private IP address was not correctly identified.");
 
-                Console.WriteLine("-----------------------------------------");
-            }
+            Console.WriteLine("-----------------------------------------");
         }
+    }
 
 
 }
