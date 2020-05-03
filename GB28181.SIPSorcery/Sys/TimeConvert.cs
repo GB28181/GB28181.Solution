8d77cffd94c2c2ff7b808b91c5f43131e197c022
lin <121553122@qq.com>
2020-05-03 11:05:59 +0800

    -

--- GB28181.SIPSorcery/Sys/TimeConvert.cs
+++ GB28181.SIPSorcery/Sys/TimeConvert.cs
@@ -15,29 +15,25 @@ namespace GB28181.SIPSorcery.Sys
         /// <returns></returns>
         public static uint DateToTimeStamp(DateTime date)
         {
-            
-            
-           // (new DateTime(1970, 1, 1, 0, 0, 0, 0).AddTicks(1588291200).Ticks* 10000000)
-
-            string aa = new DateTime(1970,1,1,0,0, 0, 0).AddTicks(1588291200).ToString("yyyy-MM-dd HH:mm:ss");
+            //return (uint)TimeZoneInfo.Local.GetUtcOffset(date).Seconds;
             return (uint)((date.ToUniversalTime().Ticks - 621355968000000000) / 10000000);
         }
 
-        private static string LongDateTimeToDateTimeString(string longDateTime)
-        {
-            //用来格式化long类型时间的,声明的变量
-            long unixDate;
-            DateTime start;
-            DateTime date;
-            //ENd
+        //private static string LongDateTimeToDateTimeString(string longDateTime)
+        //{
+        //    //用来格式化long类型时间的,声明的变量
+        //    long unixDate;
+        //    DateTime start;
+        //    DateTime date;
+        //    //ENd
 
-            unixDate = long.Parse(longDateTime);
-            start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            date = start.AddMilliseconds(unixDate).ToLocalTime();
+        //    unixDate = long.Parse(longDateTime);
+        //    start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        //    date = start.AddMilliseconds(unixDate).ToLocalTime();
 
-            return date.ToString("yyyy-MM-dd HH:mm:ss");
+        //    return date.ToString("yyyy-MM-dd HH:mm:ss");
 
-        }
+        //}
         /// <summary>
         /// 时间戳转换为日期类型
         /// 返回自1970以来的时间
@@ -46,7 +42,7 @@ namespace GB28181.SIPSorcery.Sys
         /// <returns></returns>
         //public static DateTime TimeStampToDate(uint timestamp)
         //{
-            
+
         //    return TimeZoneInfo.ConvertTimeToUtc(DateTime.Now.AddSeconds(timestamp));
         //}
     }
