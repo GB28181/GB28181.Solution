eb46ff9ed1f550232873cc167cb10688dcb98721
Edward <crazybber@outlook.com>
2020-05-28 21:30:55 +0800

    move  GrpcHttpApi src into repo

---
+++ GrpcHttpApi/README.md
@@ -0,0 +1,110 @@
+gRPC HTTP API
+=============
+
+gRPC HTTP API is an extension for ASP.NET Core that creates RESTful HTTP APIs for gRPC services. Once configured, gRPC HTTP API allows you call gRPC methods with familiar HTTP concepts:
+
+* HTTP verbs
+* URL parameter binding
+* JSON requests/responses
+
+Of course gRPC can continue to be used as well. RESTful APIs for your gRPC services. No duplication!
+
+![gRPC loves REST](grpc-rest-logo.png "gRPC loves REST")
+
+### Usage
+
+1. Add a package reference to `Microsoft.AspNetCore.Grpc.HttpApi`.
+2. Register services in *Startup.cs* with `AddGrpcHttpApi()`.
+2. Add *google/api/http.proto* and *google/api/annotations.proto* files to your project.
+3. Annotate gRPC methods in your *.proto* files with HTTP bindings and routes:
+
+```protobuf
+syntax = "proto3";
+
+import "google/api/annotations.proto";
+
+package greet;
+
+service Greeter {
+  rpc SayHello (HelloRequest) returns (HelloReply) {
+    option (google.api.http) = {
+      get: "v1/greeter/{name}"
+    };
+  }
+}
+
+message HelloRequest {
+  string name = 1;
+}
+
+message HelloReply {
+  string message = 1;
+}
+```
+
+The `SayHello` gRPC method can now be invoked as gRPC+Protobuf and as an HTTP API:
+
+* Request: `HTTP/1.1 GET /v1/greeter/world`
+* Response: `{ "message": "Hello world" }`
+
+Server logs:
+
+```
+info: Microsoft.AspNetCore.Hosting.Diagnostics[1]
+      Request starting HTTP/1.1 GET https://localhost:5001/v1/greeter/world
+info: Microsoft.AspNetCore.Routing.EndpointMiddleware[0]
+      Executing endpoint 'gRPC - v1/greeter/{name}'
+info: Server.GreeterService[0]
+      Sending hello to world
+info: Microsoft.AspNetCore.Routing.EndpointMiddleware[1]
+      Executed endpoint 'gRPC - v1/greeter/{name}'
+info: Microsoft.AspNetCore.Hosting.Diagnostics[2]
+      Request finished in 1.996ms 200 application/json
+```
+
+This is a simple example. See [HttpRule](https://cloud.google.com/service-infrastructure/docs/service-management/reference/rpc/google.api#google.api.HttpRule) for more customization options.
+
+[`Microsoft.AspNetCore.Grpc.HttpApi`](https://www.nuget.org/packages/Microsoft.AspNetCore.Grpc.HttpApi) is available on NuGet.org.
+
+### gRPC Gateway
+
+[grpc-gateway](https://github.com/grpc-ecosystem/grpc-gateway) maps RESTful HTTP APIs to gRPC using a proxy server. This project adds the same features as grpc-gateway but **without** a proxy.
+
+### Known issues
+
+##### Protobuf JSON serialization uses the JSON support in `Google.Protobuf`
+
+Issues with this JSON implementation:
+
+* It's blocking (i.e. not async), which requires the input and output to be cached in memory so as not to block ASP.NET Core.
+* It's not optimized for performance.
+    
+Improvement would be to write a new runtime serializer for protobuf types with the same behavior. It would be async, use `System.Text.Json` and cache necessary reflection. An alternative approach would be to write a `protoc` plugin that generates the JSON serialization code.
+
+##### `google/api/annotations.proto` and `google/api/http.proto` need to be added to source code
+
+`google/api/annotations.proto` and `google/api/http.proto` need to be added in the end-user's source code so the Protobuf compiler can load them along with the user's proto files. It would be a nicer developer experience if the user somehow didn't need to worry about those files.
+
+### NuGet feed
+
+The `Microsoft.AspNetCore.Grpc.HttpApi` package is published at [https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet5/nuget/v3/index.json](https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet5/nuget/v3/index.json)
+
+To use this NuGet repository and get the latest package from it, place a `NuGet.config` file with the repository setup in your solution folder:
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+    <packageSources>
+        <!-- Add this repository to the list of available repositories -->
+        <add key="DotNet5 dev repository" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet5/nuget/v3/index.json" />
+    </packageSources>
+</configuration>
+```
+
+Additional instructions for configuring a project to use a custom NuGet repository are available at [Changing NuGet configuration settings](https://docs.microsoft.com/en-us/nuget/consume-packages/configuring-nuget-behavior#changing-config-settings).
+
+### Experimental project
+
+This project is experimental. It has known issues, it is not complete and it is not supported. We are interested in this technology but there is no commitment to completing it.
+
+We want to gauge developer interest in gRPC HTTP API. If gRPC HTTP API is interesting to you then please [give feedback](https://github.com/grpc/grpc-dotnet/issues/167).
---
+++ GrpcHttpApi/Shared/ServiceDescriptorHelpers.cs
@@ -0,0 +1,304 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using Google.Api;
+using Google.Protobuf;
+using Google.Protobuf.Reflection;
+using Microsoft.AspNetCore.Routing.Patterns;
+using Microsoft.Extensions.Primitives;
+
+namespace Grpc.Shared.HttpApi
+{
+    internal static class ServiceDescriptorHelpers
+    {
+        public static ServiceDescriptor? GetServiceDescriptor(Type serviceReflectionType)
+        {
+            var property = serviceReflectionType.GetProperty("Descriptor", BindingFlags.Public | BindingFlags.Static);
+            if (property != null)
+            {
+                return (ServiceDescriptor?)property.GetValue(null);
+            }
+
+            throw new InvalidOperationException($"Get not find Descriptor property on {serviceReflectionType.Name}.");
+        }
+
+        public static bool TryResolveDescriptors(MessageDescriptor messageDescriptor, string variable, [NotNullWhen(true)]out List<FieldDescriptor>? fieldDescriptors)
+        {
+            fieldDescriptors = null;
+            var path = variable.AsSpan();
+            MessageDescriptor? currentDescriptor = messageDescriptor;
+
+            while (path.Length > 0)
+            {
+                var separator = path.IndexOf('.');
+
+                string fieldName;
+                if (separator != -1)
+                {
+                    fieldName = path.Slice(0, separator).ToString();
+                    path = path.Slice(separator + 1);
+                }
+                else
+                {
+                    fieldName = path.ToString();
+                    path = ReadOnlySpan<char>.Empty;
+                }
+
+                var field = currentDescriptor?.FindFieldByName(fieldName);
+                if (field == null)
+                {
+                    fieldDescriptors = null;
+                    return false;
+                }
+
+                if (fieldDescriptors == null)
+                {
+                    fieldDescriptors = new List<FieldDescriptor>();
+                }
+
+                fieldDescriptors.Add(field);
+                if (field.FieldType == FieldType.Message)
+                {
+                    currentDescriptor = field.MessageType;
+                }
+                else
+                {
+                    currentDescriptor = null;
+                }
+
+            }
+
+            return fieldDescriptors != null;
+        }
+
+        private static object ConvertValue(object value, FieldDescriptor descriptor)
+        {
+            switch (descriptor.FieldType)
+            {
+                case FieldType.Double:
+                    return Convert.ToDouble(value, CultureInfo.InvariantCulture);
+                case FieldType.Float:
+                    return Convert.ToSingle(value, CultureInfo.InvariantCulture);
+                case FieldType.Int64:
+                case FieldType.SInt64:
+                case FieldType.SFixed64:
+                    return Convert.ToInt64(value, CultureInfo.InvariantCulture);
+                case FieldType.UInt64:
+                case FieldType.Fixed64:
+                    return Convert.ToUInt64(value, CultureInfo.InvariantCulture);
+                case FieldType.Int32:
+                case FieldType.SInt32:
+                case FieldType.SFixed32:
+                    return Convert.ToInt32(value, CultureInfo.InvariantCulture);
+                case FieldType.Bool:
+                    return Convert.ToBoolean(value, CultureInfo.InvariantCulture);
+                case FieldType.String:
+                    return value;
+                case FieldType.Bytes:
+                    {
+                        if (value is string s)
+                        {
+                            return ByteString.FromBase64(s);
+                        }
+                        throw new InvalidOperationException("Base64 encoded string required to convert to bytes.");
+                    }
+                case FieldType.UInt32:
+                case FieldType.Fixed32:
+                    return Convert.ToUInt32(value, CultureInfo.InvariantCulture);
+                case FieldType.Enum:
+                    {
+                        if (value is string s)
+                        {
+                            var enumValueDescriptor = descriptor.EnumType.FindValueByName(s);
+                            if (enumValueDescriptor == null)
+                            {
+                                throw new InvalidOperationException($"Invalid enum value '{s}' for enum type {descriptor.EnumType.Name}.");
+                            }
+
+                            return enumValueDescriptor.Number;
+                        }
+                        throw new InvalidOperationException("String required to convert to enum.");
+                    }
+                case FieldType.Message:
+                    if (IsWrapperType(descriptor.MessageType))
+                    {
+                        return ConvertValue(value, descriptor.MessageType.FindFieldByName("value"));
+                    }
+                    break;
+            }
+
+            throw new InvalidOperationException("Unsupported type: " + descriptor.FieldType);
+        }
+
+        public static void RecursiveSetValue(IMessage currentValue, List<FieldDescriptor> pathDescriptors, object values)
+        {
+            for (var i = 0; i < pathDescriptors.Count; i++)
+            {
+                var isLast = i == pathDescriptors.Count - 1;
+                var field = pathDescriptors[i];
+
+                if (isLast)
+                {
+                    if (field.IsRepeated)
+                    {
+                        var list = (IList)field.Accessor.GetValue(currentValue);
+                        if (values is StringValues stringValues)
+                        {
+                            foreach (var value in stringValues)
+                            {
+                                list.Add(ConvertValue(value, field));
+                            }
+                        }
+                        else
+                        {
+                            list.Add(ConvertValue(values, field));
+                        }
+                    }
+                    else
+                    {
+                        if (values is StringValues stringValues)
+                        {
+                            if (stringValues.Count == 1)
+                            {
+                                field.Accessor.SetValue(currentValue, ConvertValue(stringValues[0], field));
+                            }
+                            else
+                            {
+                                throw new InvalidOperationException("Can't set multiple values onto a non-repeating field.");
+                            }
+                        }
+                        else if (values is IMessage message)
+                        {
+                            field.Accessor.SetValue(currentValue, message);
+                        }
+                        else
+                        {
+                            field.Accessor.SetValue(currentValue, ConvertValue(values, field));
+                        }
+                    }
+                }
+                else
+                {
+                    var fieldMessage = (IMessage)field.Accessor.GetValue(currentValue);
+
+                    if (fieldMessage == null)
+                    {
+                        fieldMessage = (IMessage)Activator.CreateInstance(field.MessageType.ClrType)!;
+                        field.Accessor.SetValue(currentValue, fieldMessage);
+                    }
+
+                    currentValue = fieldMessage;
+                }
+            }
+        }
+
+        internal static bool IsWrapperType(MessageDescriptor m) => m.File.Package == "google.protobuf" && m.File.Name == "google/protobuf/wrappers.proto";
+
+        public static bool TryGetHttpRule(MethodDescriptor methodDescriptor, [NotNullWhen(true)]out HttpRule? httpRule)
+        {
+            // Protobuf id of the HttpRule field
+            const int HttpRuleFieldId = 72295728;
+
+            // CustomOptions is obsolete
+            // We can use `methodDescriptor.GetOption(AnnotationsExtensions.Http)` but there
+            // is an error thrown when there is no option on the method.
+            // TODO(JamesNK): Remove obsolete code when issue is fixed. https://github.com/protocolbuffers/protobuf/issues/7127
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            return methodDescriptor.CustomOptions.TryGetMessage<HttpRule>(HttpRuleFieldId, out httpRule);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+
+        public static bool TryResolvePattern(HttpRule http, [NotNullWhen(true)]out string? pattern, [NotNullWhen(true)]out string? verb)
+        {
+            switch (http.PatternCase)
+            {
+                case HttpRule.PatternOneofCase.Get:
+                    pattern = http.Get;
+                    verb = "GET";
+                    return true;
+                case HttpRule.PatternOneofCase.Put:
+                    pattern = http.Put;
+                    verb = "PUT";
+                    return true;
+                case HttpRule.PatternOneofCase.Post:
+                    pattern = http.Post;
+                    verb = "POST";
+                    return true;
+                case HttpRule.PatternOneofCase.Delete:
+                    pattern = http.Delete;
+                    verb = "DELETE";
+                    return true;
+                case HttpRule.PatternOneofCase.Patch:
+                    pattern = http.Patch;
+                    verb = "PATCH";
+                    return true;
+                case HttpRule.PatternOneofCase.Custom:
+                    pattern = http.Custom.Path;
+                    verb = http.Custom.Kind;
+                    return true;
+                default:
+                    pattern = null;
+                    verb = null;
+                    return false;
+            }
+        }
+
+        public static Dictionary<string, List<FieldDescriptor>> ResolveRouteParameterDescriptors(RoutePattern pattern, MessageDescriptor messageDescriptor)
+        {
+            var routeParameterDescriptors = new Dictionary<string, List<FieldDescriptor>>(StringComparer.Ordinal);
+            foreach (var routeParameter in pattern.Parameters)
+            {
+                if (!TryResolveDescriptors(messageDescriptor, routeParameter.Name, out var fieldDescriptors))
+                {
+                    throw new InvalidOperationException($"Couldn't find matching field for route parameter '{routeParameter.Name}' on {messageDescriptor.Name}.");
+                }
+
+                routeParameterDescriptors.Add(routeParameter.Name, fieldDescriptors);
+            }
+
+            return routeParameterDescriptors;
+        }
+
+        public static void ResolveBodyDescriptor(string body, MethodDescriptor methodDescriptor, out MessageDescriptor? bodyDescriptor, out List<FieldDescriptor>? bodyFieldDescriptors, out bool bodyDescriptorRepeated)
+        {
+            bodyDescriptor = null;
+            bodyFieldDescriptors = null;
+            bodyDescriptorRepeated = false;
+
+            if (!string.IsNullOrEmpty(body))
+            {
+                if (!string.Equals(body, "*", StringComparison.Ordinal))
+                {
+                    if (!TryResolveDescriptors(methodDescriptor.InputType, body, out bodyFieldDescriptors))
+                    {
+                        throw new InvalidOperationException($"Couldn't find matching field for body '{body}' on {methodDescriptor.InputType.Name}.");
+                    }
+                    var leafDescriptor = bodyFieldDescriptors.Last();
+                    if (leafDescriptor.IsRepeated)
+                    {
+                        // A repeating field isn't a message type. The JSON parser will parse using the containing
+                        // type to get the repeating collection.
+                        bodyDescriptor = leafDescriptor.ContainingType;
+                        bodyDescriptorRepeated = true;
+                    }
+                    else
+                    {
+                        bodyDescriptor = leafDescriptor.MessageType;
+                    }
+                }
+                else
+                {
+                    bodyDescriptor = methodDescriptor.InputType;
+                }
+            }
+        }
+    }
+}
